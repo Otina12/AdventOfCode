11b24d6faed114eb5874fc7b1d63b800ce60ad88
GiorgiCoder <giorgiminishvili@gmail.com>
2024-12-07 09:26:28 +0400

    completed day 7 problems

---
+++ 2024/day_07/part1.py
@@ -0,0 +1,33 @@
+file = open(r"2024\Day_07\input.txt", "r")
+input = [line.strip() for line in file.readlines()]
+
+test_values = []
+numbers = []
+
+for line in input:
+    val, nums = line.split(': ')
+    test_values.append(int(val))
+    numbers.append([int(num) for num in nums.split(' ')])
+    
+true_equations = set()
+    
+def solve(line_i, i, cur_res):
+    if cur_res > test_values[line_i]:
+        return
+    if i == len(numbers[line_i]):
+        if cur_res == test_values[line_i]:
+            true_equations.add(line_i)
+        return
+    
+    solve(line_i, i + 1, cur_res + numbers[line_i][i])
+    solve(line_i, i + 1, cur_res * numbers[line_i][i])
+    
+for line_i in range(len(input)):
+    solve(line_i, 1, numbers[line_i][0])
+    
+res = 0
+
+for line_i in true_equations:
+    res += test_values[line_i]
+
+print(res)

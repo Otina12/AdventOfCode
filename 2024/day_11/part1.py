0782a27f1a16038c2cdfdabae59c471fe604ebe7
GiorgiCoder <giorgiminishvili@gmail.com>
2024-12-12 01:40:56 +0400

    completed day 11 problems

---
+++ 2024/day_11/part1.py
@@ -0,0 +1,31 @@
+file = open(r"2024\day_11\input.txt", "r")
+input = [int(num) for num in file.readline().split(' ')]
+
+blink_times = 25
+cur_stones = input
+memo = {}
+res = 0
+
+def solve(stone, step):
+    if step == blink_times:
+        return 1
+    
+    if (stone, step) in memo:
+        return memo[(stone, step)]
+    
+    memo[(stone, step)] = 0
+    digit_number = len(str(stone))
+    
+    if stone == 0:
+        memo[(stone, step)] += solve(1, step + 1)
+    elif digit_number & 1 == 0:
+        memo[(stone, step)] += solve(int(str(stone)[0:digit_number // 2]), step + 1) + solve(int(str(stone)[digit_number // 2:]), step + 1)
+    else:
+        memo[(stone, step)] += solve(stone * 2024, step + 1)
+    
+    return memo[(stone, step)]
+
+for cur_stone in cur_stones:
+    res += solve(cur_stone, 0)
+
+print(res)

d3e458f9e6294d6c46338c3c86b77146e057e316
GiorgiCoder <giorgiminishvili@gmail.com>
2024-12-14 01:59:08 +0400

    completed day 13 problems

---
+++ 2024/day_13/part2.py
@@ -0,0 +1,33 @@
+import re
+
+file = open(r"2024\day_13\input.txt", "r")
+input = file.read().split('\n\n')
+
+machines = []
+pattern = r"Button A: X\+(\d+), Y\+(\d+)\nButton B: X\+(\d+), Y\+(\d+)\nPrize: X=(\d+), Y=(\d+)"
+
+for info in input:
+    ax, ay, bx, by, x, y = map(int, re.search(pattern, info).groups())
+    machines.append((ax, ay, bx, by, x, y))
+
+def solve(machine):
+    ax, ay, bx, by, x, y = machine
+    x += 10 ** 13
+    y += 10 ** 13
+    
+    if (x * by - y * bx) % (ax * by - ay * bx) == 0:
+        k = (x * by - y * bx) // (ax * by - ay * bx)
+        
+        if (x - ax * k) % bx == 0:
+            t = (x - ax * k) // bx
+            
+            return 3 * k + t
+        
+    return 0
+
+res = 0
+
+for machine in machines:
+    res += solve(machine)
+
+print(res)

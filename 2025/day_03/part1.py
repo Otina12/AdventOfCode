f14662f0aec2799ad777b376faa30696baf843a0
Otina12 <giorgiminishvili@gmail.com>
2025-12-03 14:29:23 +0400

    improve day 3 solutions

--- 2025/day_03/part1.py
+++ 2025/day_03/part1.py
@@ -4,20 +4,12 @@ with open("input.txt", "r") as file:
     puzzle_input = [[int(num) for num in line.strip()] for line in file.readlines()]
 
 res = 0
-n = len(puzzle_input[0])
+m = len(puzzle_input[0])
 
 for powers in puzzle_input:
-    max_ten_place = powers[0]
-    max_one_place = powers[1]
-
-    for i in range(1, n - 1):
-        if powers[i] > max_ten_place:
-            max_ten_place = powers[i]
-            max_one_place = powers[i+1]
-        elif powers[i] > max_one_place:
-            max_one_place = powers[i]
-
-    max_one_place = max(max_one_place, powers[-1])
+    max_ten_place = max(powers[0:m-1])
+    max_ten_place_i = powers.index(max_ten_place)
+    max_one_place = max(powers[max_ten_place_i+1:])
 
     max_power = 10 * max_ten_place + max_one_place
     res += max_power
--- 2025/day_03/part2.py
+++ 2025/day_03/part2.py
@@ -1,29 +1,28 @@
+from collections import deque
+
 puzzle_input = []
 
 with open("input.txt", "r") as file:
     puzzle_input = [[int(num) for num in line.strip()] for line in file.readlines()]
 
-def find_leftmost_max_digit(digits, start_idx, end_idx):
-    max_num_idx, max_num = end_idx, digits[end_idx]
-
-    for i in range(end_idx, start_idx - 1, -1):
-        if digits[i] >= max_num:
-            max_num = digits[i]
-            max_num_idx = i
-    
-    return (max_num_idx, max_num)
-
 res = 0
-n = len(puzzle_input[0])
+m = len(puzzle_input[0])
+k = 12
 
 for powers in puzzle_input:
+    remove_cnt = m - k
+    stack = deque()
+
+    for power in powers:
+        while remove_cnt > 0 and stack and power > stack[-1]:
+            stack.pop()
+            remove_cnt -= 1
+        stack.append(power)
+
     max_power = 0
-    cur_start_idx = 0
-    
-    for x in range(12, 0, -1):
-        max_num_idx, max_num = find_leftmost_max_digit(powers, cur_start_idx, n - x)
-        max_power = 10 * max_power + max_num
-        cur_start_idx = max_num_idx + 1
+
+    for i in range(k):
+        max_power = max_power * 10 + stack[i]
 
     res += max_power
 
